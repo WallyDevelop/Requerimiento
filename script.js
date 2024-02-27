bc69c6cc5834d1c8a3817e778d442983f8410709
WallyDevelop <161369548+WallyDevelop@users.noreply.github.com>
2024-02-27 08:26:33 -0500

    Primer Documento

---
+++ script.js
@@ -0,0 +1,3 @@
+alert("Welcome to SaVa Clothing Store")
+alert("Proyecto Formativo de Walberto Aguilar Asprilla")
+alert("Este proyecto es un ecommerce para mi esposa, quien es una vendedora de ropa independiente. Lo voy a realizar de manera individual debido a que no cuento con el tiempo para trabajar en equipo")
